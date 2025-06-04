6069657d62f586fcdf92d240f2dcfb745a78d4de
Kishore Piratheepan <kpirathe@uwo.ca>
2025-06-04 00:29:11 -0400

    fixed responsiviness

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import NavBar from './components/Navbar';
-import Footer from './components/Footer'
-import './components/index.css'
+import Footer from './components/Footer';
+import './components/index.css';
 
 function App() {
-    return (
-        <BrowserRouter basename='/portfolio-website'>
+  return (
+    <BrowserRouter basename='/portfolio-website'>
+      <div className="page-wrapper">
         <NavBar />
-        <Routes>
-            <Route path="/" element= {<Home />} />
-            <Route path="/projects" element= {<Projects />} />
-            <Route path="/contact" element= {<Contact />} />
-        </Routes>
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
         <Footer />
-        </BrowserRouter>
-    );
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
